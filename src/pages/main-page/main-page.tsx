dea34a53e91d8f2712f1b457eeba2e1fff8b1266
Artem Krawtsow <artemk@asist-lab.com>
2024-02-10 01:13:24 +0300

    add: base html and styles, add assets

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
-import { Button, Drawer, Layout, Menu, Space, theme } from 'antd';
+import { Button, Card, Divider, Drawer, Layout, Menu, Space, theme } from 'antd';
 import {
+    AndroidFilled,
+    AppleFilled,
+    AppleOutlined,
+    CalendarTwoTone,
+    HeartFilled,
+    IdcardTwoTone,
     MenuFoldOutlined,
     MenuUnfoldOutlined,
+    SettingOutlined,
     UploadOutlined,
     UserOutlined,
     VideoCameraOutlined,
 } from '@ant-design/icons';
+import Main_page_light from "../../assets/imgs/Main_page_light.png"
 
 import s from './main-page.module.scss';
 
@@ -38,7 +46,7 @@ export const MainPage: React.FC = () => {
         <>
 
             <Layout hasSider>
-                <Sider
+                {/* <Sider
                     trigger={null}
                     collapsed={collapsed}
                     // breakpoint="lg"
@@ -52,7 +60,7 @@ export const MainPage: React.FC = () => {
                     style={{
                         overflow: 'auto',
                         height: '100dvh',
-                        position: 'fixed', 
+                        position: 'fixed',
                         left: 0,
                         top: 0,
                         bottom: 0
@@ -60,9 +68,14 @@ export const MainPage: React.FC = () => {
                 >
                     <div className="demo-logo-vertical" />
                     <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
-                </Sider>
+                </Sider> */}
                 <Layout style={{
                     // position: 'relative'
+                    minHeight: "100dvh",
+                    backgroundPosition: "center",
+                    backgroundImage: `url(${Main_page_light})`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
                 }}>
                     <Button
                         type="text"
@@ -86,22 +99,148 @@ export const MainPage: React.FC = () => {
                         </Space>
                     </Button>
                     <Header style={{
-                        padding: 0,
+                        height: "168px",
+                        padding: "16px 24px",
+                        display: "flex",
+                        flexDirection: "column",
+                        backgroundColor: "#F0F5FF"
                         // background: colorBgContainer 
-                    }}>Header</Header>
-                    <Content style={{ margin: '24px 16px 0', overflow: 'initial'}}>
+                    }}>
+                        <div
+                            style={{
+                                fontWeight: 400,
+                                fontSize: "14px",
+                                lineHeight: "18.2px"
+                            }}
+                        >Главная</div>
+                        <div
+                            style={{
+                                display: "flex",
+                                justifyContent: "space-between",
+                                paddingTop: "8px",
+                            }}
+                        >
+                            <div
+                                style={{
+                                    fontWeight: 700,
+                                    fontSize: "38px",
+                                    display: "flex",
+                                    padding: "6px 0",
+                                    flexDirection: "column",
+                                    lineHeight: "49.4px"
+                                }}
+                            >
+
+                                <p>
+                                    {"Приветствуем тебя в CleverFit — приложении,"}
+                                </p>
+                                <p>
+                                    {"которое поможет тебе добиться своей мечты!"}
+                                </p>
+                            </div>
+                            <div
+                                style={{
+                                    width: "129px",
+                                    fontWeight: 400,
+                                    fontSize: "14px",
+                                    lineHeight: "18.2px"
+                                }}
+                            >
+
+                                <Button icon={<SettingOutlined />} style={{ color: "black" }} type="link">Настройки</Button>
+                            </div>
+                        </div>
+                    </Header>
+                    <Content style={{ overflow: 'initial' }}>
 
                         <div
                             style={{
                                 position: "relative",
+                                maxWidth: "800px",
                                 padding: 24,
+                                display: "flex",
+                                flexDirection: "column",
+                                gap: "24px"
                                 // minHeight: 360,
                                 // background: colorBgContainer,
                                 // borderRadius: borderRadiusLG,
                             }}
                         >
+                            <Card style={{
+                                fontSize: "16px",
+                                lineHeight: "20.8px"
+                            }}>
+                                C CleverFit ты сможешь:<br />
+                                — планировать свои тренировки на календаре, выбирая тип и уровень нагрузки;<br />
+                                — отслеживать свои достижения в разделе статистики, сравнивая свои результаты c нормами и рекордами;<br />
+                                — создавать свой профиль, где ты можешь загружать свои фото, видео и отзывы o тренировках;<br />
+                                — выполнять расписанные тренировки для разных частей тела, следуя подробным инструкциям и советам профессиональных тренеров.
+                            </Card>
+                            <div style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                gap: "16px"
+                            }}>
+                                <Card style={{
+                                    fontSize: "20px",
+                                    lineHeight: "26px",
+                                    fontWeight: 500,
+                                }}>
+                                    <p>{"CleverFit — это не просто приложение, а твой личный помощник в мире фитнеса. Не откладывай на завтра — начни тренироваться уже сегодня!"}</p>
+                                </Card>
+                                <div
+                                    style={{
+                                        display: "flex",
+                                        gap: "16px"
+                                    }}
+                                >
+                                    <Card
+                                        style={{
+                                            width: "240px"
+                                        }}
+                                        size='small'
+                                        title="Расписать тренировки"
+                                        headStyle={{ display: "flex", alignItems: "center", justifyContent: "center", height: "45px" }}
+                                        bodyStyle={{ textAlign: "center", height: "56px", padding: "12px" }}
+                                        bordered={false} >
+                                        <Button type='link'>
+                                            <HeartFilled />
+                                            Тренировки
+                                        </Button>
+                                    </Card>
+                                    <Card
+                                        style={{
+                                            width: "240px"
+                                        }}
+                                        size='small'
+                                        title="Назначить календарь"
+                                        headStyle={{ display: "flex", alignItems: "center", justifyContent: "center", height: "45px" }}
+                                        bodyStyle={{ textAlign: "center", height: "56px", padding: "12px" }}
+                                        bordered={false} >
+                                        <Button type='link'>
+                                            <CalendarTwoTone />
+                                            Календарь
+                                        </Button>
+                                    </Card>
+                                    <Card
+                                        style={{
+                                            width: "240px"
+                                        }}
+                                        size='small'
+                                        title="Заполнить Профиль"
+                                        headStyle={{ display: "flex", alignItems: "center", justifyContent: "center", height: "45px" }}
+                                        bodyStyle={{ textAlign: "center", height: "56px", padding: "12px" }}
+                                        bordered={false} >
+                                        <Button type='link'>
+                                            <IdcardTwoTone />
+                                            Профиль
+                                        </Button>
+                                    </Card>
+                                </div>
+                            </div>
 
-                            <p>long content</p>
+
+                            {/* <p>long content</p>
                             {
                                 // indicates very long content
                                 Array.from({ length: 100 }, (_, index) => (
@@ -110,48 +249,56 @@ export const MainPage: React.FC = () => {
                                         <br />
                                     </React.Fragment>
                                 ))
-                            }
+                            } */}
                         </div>
                     </Content>
-                    <Footer style={{ textAlign: 'center' }}>
-                        Ant Design ©{new Date().getFullYear()} Created by Ant UED
+                    <Footer style={{ padding: "42px 24px", background: "none", display: "flex", alignItems: "flex-end", justifyContent: "space-between" }}>
+                        <Button type='link'>
+                            Смотреть отзывы
+                        </Button>
+                        <Card
+                            style={{
+                                width: "240px"
+                            }}
+                            size='small'
+                            title={
+                                <div style={{
+                                    boxSizing: "border-box",
+                                    padding: "0",
+                                    display: 'flex',
+                                    gap: "8px",
+                                    flexDirection: "column",
+                                }}>
+                                    <Button style={{ textAlign: "start", padding: "0 20px", height: "21px", fontSize: "16" }} type='link'>Скачать на телефон</Button>
+                                    <Button style={{ textAlign: "start", padding: "0 20px", height: "18px", fontSize: "16" }} type='link' disabled>Доступно в про-тарифу</Button>
+                                </div>
+                            }
+                            headStyle={{ height: "71px", padding: "4px", }}
+                            bodyStyle={{ textAlign: "center", height: "56px", padding: "12px 0", }}
+                            bordered={false} >
+                            <Button style={{ color: "black", width: "50%" }} type='link' color='balck'>
+                                <AndroidFilled />
+                                Android
+                            </Button>
+                            <Button style={{ color: "black", width: "50%" }} type='link' color='balck'>
+                                <AppleFilled />
+                                IOS
+                            </Button>
+                        </Card>
+                        {/* <div style={{ display: 'flex', flexDirection: "column" }}>
+                            <div style={{ display: 'flex', flexDirection: "column" }}>
+                                <div>Скачать на телефон</div>
+                                <div>Доступно в про-тарифу</div>
+                            </div>
+                            <Divider />
+                            <div style={{ display: 'flex' }}>
+                                <div>Android</div>
+                                <div>IOS</div>
+                            </div>
+                        </div> */}
                     </Footer>
                 </Layout>
-            </Layout>
-
-            {/* <div className={s.mainContainer}>
-                <nav
-                    style={{ width: 300, height: 300, background: "#f00" }}
-                    className={s.width200} id='nav-container'>
-                </nav>
-                <div className='pageContent'>
-                    <Button type="primary" onClick={onToggle}>
-                        Open
-                    </Button>
-                    <div className='header'>header</div>
-                    <div className='content'>content</div>
-                    <div className='footer'>footer</div>
-                </div>
-            </div>
-            <Drawer
-                getContainer="#nav-container"
-                mask={false}
-                closable={false}
-                title="Basic Drawer"
-                placement="left"
-                onClose={onClose} open={open}
-            >
-                <div className={s.relative}>
-                    <div className={s.toggleBtn}>
-                        <Button type="primary" onClick={onToggle}>
-                            Open
-                        </Button>
-                    </div>
-                    <p>Some contents1...</p>
-                    <p>Some contents...</p>
-                    <p>Some contents...</p>
-                </div>
-            </Drawer> */}
+            </Layout >
         </>
     );
 };
