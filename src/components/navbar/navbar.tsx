f739da95cf56eae302a4a920f64249f6c2198fbd
Artem Krawtsow <artemk@asist-lab.com>
2024-02-13 00:11:27 +0300

    feat: add navbar, add styles for all components on main page

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,276 @@
+import { Button, Drawer, Grid, Image, Layout, Menu, Space } from 'antd';
+import {
+
+  CalendarFilled,
+  CalendarOutlined,
+  CalendarTwoTone,
+  HeartFilled,
+  IdcardFilled,
+  IdcardOutlined,
+  IdcardTwoTone,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  TrophyFilled,
+
+} from '@ant-design/icons';
+import React from 'react';
+import LogoFullSVG from "@assets/icons/logo-full.svg"
+import LogoCollapsedSVG from "@assets/icons/logo-collapsed.svg"
+import IconExit from "@assets/icons/icon-exit.svg"
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState, setCollapsed } from '@redux/configure-store';
+import s from './navbar.module.scss';
+
+export const Navbar = () => {
+  const { useBreakpoint } = Grid;
+  const screens = useBreakpoint();
+  const collapsed = useSelector((state: RootState) => state.app.collapsed);
+  const dispatch = useDispatch();
+
+  const { Sider } = Layout;
+
+
+  const navItems = [
+    {
+      icon: CalendarOutlined,
+      name: 'Календарь',
+      href: '',
+    },
+    {
+      icon: HeartFilled,
+      name: 'Тренировки',
+      href: '',
+    },
+    {
+      icon: TrophyFilled,
+      name: 'Достижения',
+      href: '',
+    },
+    {
+      icon: IdcardOutlined,
+      name: 'Профиль',
+      href: '',
+    },
+  ]
+
+  const items = navItems.map(
+    (item, index) => ({
+      key: String(index + 1),
+      icon: React.createElement(item.icon),
+      label: item.name,
+    }),
+  );
+
+
+  const setNavCollapsed = (value: boolean) => {
+    dispatch(setCollapsed(value));
+  };
+
+  return (
+    <>
+      <Sider
+        trigger={null}
+        width={208}
+        collapsed={collapsed}
+        collapsedWidth="64"
+        collapsible
+        // onBreakpoint={(broken) => {
+        //   console.log(broken);
+        // }}
+        // onCollapse={(collapsed, type) => {
+        //   console.log(collapsed, type);
+        // }}
+        style={{
+          display: (!screens?.xs) ? "block" : "none",
+          backgroundColor: "white",
+          overflow: "visible",
+          height: '100dvh',
+          position: 'fixed',
+          left: 0,
+          top: 0,
+          bottom: 0,
+          zIndex: 1000
+        }}
+      >
+        <div
+          style={{ display: "flex", width: "100%", height: "144px", justifyContent: "center" }}
+        >
+
+          {collapsed ?
+            <Image
+              style={{ marginTop: "52px", transition: "width 0.5s ease-in-out, margin-top 0.5s ease-in-out" }}
+              width={29}
+              preview={false}
+              src={LogoCollapsedSVG}
+              alt="Logo"
+            />
+            :
+            <Image
+              style={{ marginTop: "44px", transition: "width 0.5s ease-in-out, margin-top 0.5s ease-in-out" }}
+
+              width={133}
+              preview={false}
+              src={LogoFullSVG}
+              alt="Logo"
+            />
+          }
+        </div>
+        <Menu
+          className={s.icons}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "16px"
+          }}
+          theme="light" mode="inline" defaultSelectedKeys={['0']} items={items} >
+
+        </Menu>
+        <Button
+          style={{
+            paddingLeft: "24px",
+            display: "flex",
+            alignItems: "center",
+            position: "absolute",
+            bottom: 0,
+            color: "#262626"
+          }}
+          type='link'
+        >
+          <Image
+            width={14}
+            preview={false}
+            src={IconExit}
+            alt="Exit"
+          />
+          <span style={{
+            paddingLeft: "25px",
+            display: collapsed ? "none" : "block",
+          }}>
+            Выход
+          </span>
+        </Button>
+        <Button
+          data-test-id='sider-switch'
+          type="text"
+          onClick={() => setNavCollapsed(!collapsed)}
+          style={{
+            zIndex: 1001,
+            position: "absolute",
+            right: "-20px",
+            top: 'calc(50vh - 64px - 24px)',
+            fontSize: '16px',
+            padding: 0,
+            width: "0",
+            borderLeft: "20px solid white",
+            borderBottom: "5px solid transparent",
+            borderTop: "5px solid transparent",
+            height: 66,
+          }}
+        >
+          <Space style={{ position: "relative", left: "-18px" }}>
+            {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+          </Space>
+        </Button>
+      </Sider>
+      <div
+        style={{
+          display: (screens.xs) ? "block" : "none",
+          width: 0,
+          overflow: "visible",
+          position: "fixed",
+          top: 0,
+          left: 0,
+          height: "100dvh",
+          zIndex: "1002"
+        }}
+
+
+      >
+        <Button
+          data-test-id='sider-switch-mobile'
+          type="text"
+          onClick={() => setNavCollapsed(!collapsed)}
+          style={{
+            zIndex: 1001,
+            position: "absolute",
+            left: collapsed ? "0" : "105px",
+            top: '24px',
+            fontSize: '16px',
+            padding: 0,
+            width: "0",
+            borderLeft: "32px solid white",
+            borderBottom: "5px solid transparent",
+            borderTop: "5px solid transparent",
+            height: 48,
+          }}
+        >
+          <Space style={{ position: "relative", left: "-24px" }}>
+            {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+          </Space>
+        </Button>
+        <Drawer
+          mask={false}
+          style={{
+            display: (screens?.xs) ? "block" : "none",
+          }}
+          placement={"left"}
+          closable={false}
+          onClose={() => setNavCollapsed(false)}
+          open={!collapsed}
+          key={"left"}
+          width={106}
+          bodyStyle={{
+            padding: "0 8px",
+          }}
+
+        >
+          <Image
+            style={{ margin: "16px 9px" }}
+            width={72}
+            preview={false}
+            src={LogoFullSVG}
+            alt="Logo"
+          />
+          <Menu
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "8px",
+              border: 0
+            }}
+            theme="light" mode="inline" defaultSelectedKeys={['new']} >
+            {items.map((item, index) => (
+              <Menu.Item
+                style={{
+                  height: "42px",
+                  margin: 0,
+                  padding: 0,
+                }}
+                key={index} className={index === items.length - 1 ? 'last-menu-item' : ''}>
+                {item.label}
+              </Menu.Item>
+            ))}
+          </Menu>
+          <Button
+            style={{
+              width: "106px",
+              paddingLeft: "24px",
+              display: "flex",
+              alignItems: "center",
+              position: "absolute",
+              bottom: 0,
+              color: "black"
+            }}
+            type='link'
+          >
+            <span style={{
+              display: collapsed ? "none" : "block",
+            }}>
+              Выход
+            </span>
+          </Button>
+        </Drawer>
+      </div>
+    </>
+  )
+}
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,302 +1,45 @@
-import React, { useState } from 'react';
-import { Button, Card, Divider, Drawer, Layout, Menu, Space, theme } from 'antd';
-import {
-    AndroidFilled,
-    AppleFilled,
-    AppleOutlined,
-    CalendarTwoTone,
-    HeartFilled,
-    IdcardTwoTone,
-    MenuFoldOutlined,
-    MenuUnfoldOutlined,
-    SettingOutlined,
-    UploadOutlined,
-    UserOutlined,
-    VideoCameraOutlined,
-} from '@ant-design/icons';
+import React from 'react';
+import { Grid, Layout } from 'antd';
+
 import Main_page_light from "../../assets/imgs/Main_page_light.png"
 
 import s from './main-page.module.scss';
-
-const layoutStyle = {
-    borderRadius: 8,
-    overflow: 'hidden',
-    height: "100dvh",
-    // width: 'calc(50% - 8px)',
-    // maxWidth: 'calc(50% - 8px)',
-};
-
+import { Navbar } from '@components/navbar';
+import { MainHeader } from './header/main-header';
+import { MainContent } from './content/main-content';
+import { MainFooter } from './footer/main-footer';
+import useWindowWidth from '@hooks/use-window-width';
+import { useSelector } from 'react-redux';
 
 export const MainPage: React.FC = () => {
-    const { Header, Footer, Sider, Content } = Layout;
+    const collapsed = useSelector((state: any) => state.app.collapsed);
+    const { useBreakpoint } = Grid;
+    const screens = useBreakpoint();
+
     // const {
     //     token: { colorBgContainer, borderRadiusLG },
     // } = theme.useToken();
-    const [collapsed, setCollapsed] = useState(false);
+    const layoutPaddingLeft = (screens?.xs) ? '0' : (collapsed ? '64px' : '208px');
 
-    const items = [UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
-        (icon, index) => ({
-            key: String(index + 1),
-            icon: React.createElement(icon),
-            label: `nav ${index + 1}`,
-        }),
-    );
 
     return (
         <>
-
-            <Layout hasSider>
-                {/* <Sider
-                    trigger={null}
-                    collapsed={collapsed}
-                    // breakpoint="lg"
-                    collapsedWidth="30"
-                    onBreakpoint={(broken) => {
-                        console.log(broken);
-                    }}
-                    onCollapse={(collapsed, type) => {
-                        console.log(collapsed, type);
-                    }}
-                    style={{
-                        overflow: 'auto',
-                        height: '100dvh',
-                        position: 'fixed',
-                        left: 0,
-                        top: 0,
-                        bottom: 0
-                    }}
-                >
-                    <div className="demo-logo-vertical" />
-                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
-                </Sider> */}
-                <Layout style={{
-                    // position: 'relative'
+            <Layout style={{ position: "relative" }}>
+                <Navbar />
+                <Layout className={s.layout} style={{
+                    position: 'relative',
+                    transition: "padding-left 0.146s linear",
+                    paddingLeft: layoutPaddingLeft,
                     minHeight: "100dvh",
                     backgroundPosition: "center",
                     backgroundImage: `url(${Main_page_light})`,
                     backgroundSize: "cover",
                     backgroundRepeat: "no-repeat",
+
                 }}>
-                    <Button
-                        type="text"
-                        onClick={() => setCollapsed(!collapsed)}
-                        style={{
-                            position: "absolute",
-                            left: 0,
-                            top: 'calc(50vh - 64px - 24px)',
-                            fontSize: '16px',
-                            padding: 0,
-                            width: "0",
-                            borderLeft: "20px solid red",
-                            borderBottom: "5px solid transparent",
-                            borderTop: "5px solid transparent",
-                            // width: 64,
-                            height: 64,
-                        }}
-                    >
-                        <Space style={{ position: "relative", left: "-15px" }}>
-                            {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-                        </Space>
-                    </Button>
-                    <Header style={{
-                        height: "168px",
-                        padding: "16px 24px",
-                        display: "flex",
-                        flexDirection: "column",
-                        backgroundColor: "#F0F5FF"
-                        // background: colorBgContainer 
-                    }}>
-                        <div
-                            style={{
-                                fontWeight: 400,
-                                fontSize: "14px",
-                                lineHeight: "18.2px"
-                            }}
-                        >Главная</div>
-                        <div
-                            style={{
-                                display: "flex",
-                                justifyContent: "space-between",
-                                paddingTop: "8px",
-                            }}
-                        >
-                            <div
-                                style={{
-                                    fontWeight: 700,
-                                    fontSize: "38px",
-                                    display: "flex",
-                                    padding: "6px 0",
-                                    flexDirection: "column",
-                                    lineHeight: "49.4px"
-                                }}
-                            >
-
-                                <p>
-                                    {"Приветствуем тебя в CleverFit — приложении,"}
-                                </p>
-                                <p>
-                                    {"которое поможет тебе добиться своей мечты!"}
-                                </p>
-                            </div>
-                            <div
-                                style={{
-                                    width: "129px",
-                                    fontWeight: 400,
-                                    fontSize: "14px",
-                                    lineHeight: "18.2px"
-                                }}
-                            >
-
-                                <Button icon={<SettingOutlined />} style={{ color: "black" }} type="link">Настройки</Button>
-                            </div>
-                        </div>
-                    </Header>
-                    <Content style={{ overflow: 'initial' }}>
-
-                        <div
-                            style={{
-                                position: "relative",
-                                maxWidth: "800px",
-                                padding: 24,
-                                display: "flex",
-                                flexDirection: "column",
-                                gap: "24px"
-                                // minHeight: 360,
-                                // background: colorBgContainer,
-                                // borderRadius: borderRadiusLG,
-                            }}
-                        >
-                            <Card style={{
-                                fontSize: "16px",
-                                lineHeight: "20.8px"
-                            }}>
-                                C CleverFit ты сможешь:<br />
-                                — планировать свои тренировки на календаре, выбирая тип и уровень нагрузки;<br />
-                                — отслеживать свои достижения в разделе статистики, сравнивая свои результаты c нормами и рекордами;<br />
-                                — создавать свой профиль, где ты можешь загружать свои фото, видео и отзывы o тренировках;<br />
-                                — выполнять расписанные тренировки для разных частей тела, следуя подробным инструкциям и советам профессиональных тренеров.
-                            </Card>
-                            <div style={{
-                                display: "flex",
-                                flexDirection: "column",
-                                gap: "16px"
-                            }}>
-                                <Card style={{
-                                    fontSize: "20px",
-                                    lineHeight: "26px",
-                                    fontWeight: 500,
-                                }}>
-                                    <p>{"CleverFit — это не просто приложение, а твой личный помощник в мире фитнеса. Не откладывай на завтра — начни тренироваться уже сегодня!"}</p>
-                                </Card>
-                                <div
-                                    style={{
-                                        display: "flex",
-                                        gap: "16px"
-                                    }}
-                                >
-                                    <Card
-                                        style={{
-                                            width: "240px"
-                                        }}
-                                        size='small'
-                                        title="Расписать тренировки"
-                                        headStyle={{ display: "flex", alignItems: "center", justifyContent: "center", height: "45px" }}
-                                        bodyStyle={{ textAlign: "center", height: "56px", padding: "12px" }}
-                                        bordered={false} >
-                                        <Button type='link'>
-                                            <HeartFilled />
-                                            Тренировки
-                                        </Button>
-                                    </Card>
-                                    <Card
-                                        style={{
-                                            width: "240px"
-                                        }}
-                                        size='small'
-                                        title="Назначить календарь"
-                                        headStyle={{ display: "flex", alignItems: "center", justifyContent: "center", height: "45px" }}
-                                        bodyStyle={{ textAlign: "center", height: "56px", padding: "12px" }}
-                                        bordered={false} >
-                                        <Button type='link'>
-                                            <CalendarTwoTone />
-                                            Календарь
-                                        </Button>
-                                    </Card>
-                                    <Card
-                                        style={{
-                                            width: "240px"
-                                        }}
-                                        size='small'
-                                        title="Заполнить Профиль"
-                                        headStyle={{ display: "flex", alignItems: "center", justifyContent: "center", height: "45px" }}
-                                        bodyStyle={{ textAlign: "center", height: "56px", padding: "12px" }}
-                                        bordered={false} >
-                                        <Button type='link'>
-                                            <IdcardTwoTone />
-                                            Профиль
-                                        </Button>
-                                    </Card>
-                                </div>
-                            </div>
-
-
-                            {/* <p>long content</p>
-                            {
-                                // indicates very long content
-                                Array.from({ length: 100 }, (_, index) => (
-                                    <React.Fragment key={index}>
-                                        {index % 20 === 0 && index ? 'more' : '...'}
-                                        <br />
-                                    </React.Fragment>
-                                ))
-                            } */}
-                        </div>
-                    </Content>
-                    <Footer style={{ padding: "42px 24px", background: "none", display: "flex", alignItems: "flex-end", justifyContent: "space-between" }}>
-                        <Button type='link'>
-                            Смотреть отзывы
-                        </Button>
-                        <Card
-                            style={{
-                                width: "240px"
-                            }}
-                            size='small'
-                            title={
-                                <div style={{
-                                    boxSizing: "border-box",
-                                    padding: "0",
-                                    display: 'flex',
-                                    gap: "8px",
-                                    flexDirection: "column",
-                                }}>
-                                    <Button style={{ textAlign: "start", padding: "0 20px", height: "21px", fontSize: "16" }} type='link'>Скачать на телефон</Button>
-                                    <Button style={{ textAlign: "start", padding: "0 20px", height: "18px", fontSize: "16" }} type='link' disabled>Доступно в про-тарифу</Button>
-                                </div>
-                            }
-                            headStyle={{ height: "71px", padding: "4px", }}
-                            bodyStyle={{ textAlign: "center", height: "56px", padding: "12px 0", }}
-                            bordered={false} >
-                            <Button style={{ color: "black", width: "50%" }} type='link' color='balck'>
-                                <AndroidFilled />
-                                Android
-                            </Button>
-                            <Button style={{ color: "black", width: "50%" }} type='link' color='balck'>
-                                <AppleFilled />
-                                IOS
-                            </Button>
-                        </Card>
-                        {/* <div style={{ display: 'flex', flexDirection: "column" }}>
-                            <div style={{ display: 'flex', flexDirection: "column" }}>
-                                <div>Скачать на телефон</div>
-                                <div>Доступно в про-тарифу</div>
-                            </div>
-                            <Divider />
-                            <div style={{ display: 'flex' }}>
-                                <div>Android</div>
-                                <div>IOS</div>
-                            </div>
-                        </div> */}
-                    </Footer>
+                    <MainHeader />
+                    <MainContent />
+                    <MainFooter />
                 </Layout>
             </Layout >
         </>
