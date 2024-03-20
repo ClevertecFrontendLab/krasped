f44dc52e33180b79ad56f379656e619e856ce708
Artem Krawtsow <91025633+krasped@users.noreply.github.com>
2024-03-20 10:17:07 +0300

    fix : additional consts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -9,6 +9,7 @@ export const _AuthRegistration = "/auth/registration";
 export const _AuthLogin = "/auth/login";
 export const _AuthConfirmEmail = "/auth/confirm-email";
 
+export const _Result = "/result";
 export const _SuccessChangePassword = "/result/success-change-password";
 export const _ErrorChangePassword = "/result/error-change-password";
 export const _ErrorLogin = "/result/error-login";
